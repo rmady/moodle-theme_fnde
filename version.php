6fe3f23ca61e3ca65a88a1d54d216b4991a8c3a5
Rodrigo Mady da Silva <rodrigo.mady@gmail.com>
2021-12-10 09:17:26 -0300

    [UPDATE] Update version

--- version.php
+++ version.php
@@ -12,6 +12,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_fnde';
 $plugin->release   = '1.0.0';
-$plugin->version   = 2021072100;
+$plugin->version   = 2021101800;
 $plugin->requires  = 2020110900;
 $plugin->maturity  = MATURITY_STABLE;
